316a5abea073d17157601a11034d2ef42fb26660
KienLeTrung <kienletrung.1995@gmail.com>
2016-12-22 00:28:32 +0700

    Chuyen Game thanh thuoc tinh trong Server5

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,12 +2,20 @@ package server;
 
 import java.net.DatagramPacket;
 
+import game.Game;
+
 public class Server extends Thread {
 	private ServerIO io;
+	private Game game;
 	public Server () {
 		this.io = new ServerIO();
 	}
   public void run() {
+	  this.game = new Game();
+	  game.start();
+	  game.isServer = true;
+	  game.isClient = false;
+	  game.start();
 	  while (true) {
 		  byte[] data = new byte[1024];
 		  DatagramPacket packet = new DatagramPacket(data, data.length);
@@ -16,6 +24,8 @@ public class Server extends Thread {
 			  System.out.println("Client < " + packet.getAddress().toString() + ":" + packet.getPort());
 			  DatagramPacket res = new DatagramPacket("pong".getBytes(), "pong".getBytes().length, packet.getAddress(), packet.getPort());
 			  io.sendPacket(res);  
+			  
+			  
 		  }
 	  }
   }
